0a7e77c91d08d4baa1503cb69d7de689b93afb34
joeycalt <joeycalt@yahoo.com>
2022-08-09 21:04:29 -0400

    counting how many flips player takes and place shuffled array into individual div cells. still trying to figure out how to hide the emojis and only show when div cell is clicked

--- matching.js
+++ matching.js
@@ -1,5 +1,5 @@
 const createBoard= document.querySelector('.gameboard');
-const gameboardWrapper = document.querySelector('#gameboard-wrapper');
+const wrappers = document.querySelector('#gameboard-wrapper');
 const cells = document.querySelectorAll('#cell');
 const resetButton = document.querySelector ('.reset');
 let gameOn = true;
@@ -16,14 +16,28 @@ let cardsWon = [];
 let count = 0;
 let cardsSelectedId = []; 
 let cardsChosen = [];
-let firstClick = [];
-let secondClick = [];
+let firstClick;
+let secondClick;
 let clicksCounter = 0;
 let isFlipped = false;
 let lock = false;
 letFlipsBox = document.querySelector('.flips-box')
-
-
+let boxZero = document.querySelector('.box_zero')
+let boxOne = document.querySelector('.box_one')
+let boxTwo = document.querySelector('.box_two')
+let boxThree = document.querySelector('.box_three')
+let boxFour = document.querySelector('.box_four')
+let boxFive = document.querySelector('.box_five')
+let boxSix = document.querySelector('.box_six')
+let boxSeven = document.querySelector('.box_seven')
+let boxEight = document.querySelector('.box_eight')
+let boxNine = document.querySelector('.box_nine')
+let boxTen = document.querySelector('.box_ten')
+let boxEleven = document.querySelector('.box_eleven')
+let boxTwelve = document.querySelector('.box_twelve')
+let boxThirteen = document.querySelector('.box_thirteen')
+let boxFourteen = document.querySelector('.box_fourteen')
+let boxFifteen = document.querySelector('.box_fifteen')
 // __________________________________________________________________//
 // CODE:
 // |
@@ -31,11 +45,34 @@ letFlipsBox = document.querySelector('.flips-box')
 // |
 // v
 
-gameboardWrapper.addEventListener('click', function () {
+function move(e) {
+    currentFace = e.target.dataset.card
+    console.log(emojis[currentEmoji])
+    if (e.target.innerHTML === 'face');
+} 
+
+
+
+
+// reset game and page
+resetButton.onclick = reloadPage;
+
+function reloadPage() {
+    window.location.reload();
+}
+
+
+
+
+
+// flips counter
+wrappers.addEventListener('click', function () {
     count += 1;
     document.querySelector('.flips-box').innerText = count;
     console.log(count);
 });
+
+
 
 
 
@@ -54,93 +91,68 @@ document.querySelector('.start').addEventListener('click', function() {
             clearInterval(counter);
         }
     }, 1000);
+}, {once: true})
 
 
 
 
 
-// choose card 
-function move(evt) {
-    currentFace = evt.target.dataset.card
-    // console.log(emojis[currentEmoji])
-    if (evt.target.innerHTML === 'face') {
 
-    } 
-} 
-
-//shuffle array of emojis
+// shuffle array of emojis
 let shuffledEmojis = emojis.sort(function () {
     return Math.random() - 0.5;
 });
 console.log(shuffledEmojis);
 
-const tiles = [...document.querySelectorAll('#cells')];
-for(const tile of tiles) {
-    console.log(tile)
-    tile.addEventListener('click', viewCard)
-}
+
+let newEmojis = shuffledEmojis;
+cells.forEach((cell) => {
+    boxZero.innerHTML = newEmojis[0];
+    boxOne.innerHTML = newEmojis[1];
+    boxTwo.innerHTML = newEmojis[2];
+    boxThree.innerHTML = newEmojis[3];
+    boxFour.innerHTML = newEmojis[4];
+    boxFive.innerHTML = newEmojis[5];
+    boxSix.innerHTML = newEmojis[6];
+    boxSeven.innerHTML = newEmojis[7];
+    boxEight.innerHTML = newEmojis[8];
+    boxNine.innerHTML = newEmojis[9];
+    boxTen.innerHTML = newEmojis[10];
+    boxEleven.innerHTML = newEmojis[11];
+    boxTwelve.innerHTML = newEmojis[12];
+    boxThirteen.innerHTML = newEmojis[13];
+    boxFourteen.innerHTML = newEmojis[14];
+    boxFifteen.innerHTML = newEmojis[15];
+
+}) 
+
+
 
 
 // keep cards matched and return cards not matched
-
-cells.forEach((cell)=> {
-    currentEmoji = 
-    cell.addEventListener('click', () => {
-        cell.classList.add('clicked');
-
-        if(counter === 0) {
-            firstClick = shuffledEmojis[''];
-            counter++;
-        } else {
-            secondClick = shuffledEmojis[''];
-            counter = 0;
-        }
+function flipCard () {
+    this.classList.add('flip');
+    
+    if(!isFlipped) {
+        isFlipped = true;
+        firstClick = this;
+        return;
+    } else {
+        isFlipped = false;
+        secondClick = this;
         
-        if(firstClick === secondClick) {
-            const correctCells = document.querySelectorAll(
-                ".cell[shuffledEmojis='" + firstClick + "']"
-                );
-                correctCells[0].classList.add('checked');
-                correctCells[0].classList.remove('clicked');
-                correctCells[1].classList.add('checked');
-                correctCells[1].classList.remove('clicked');
-            }
+        if (firstClick === secondClick) {
+            firstClick.removeEventListener('click', flipCard);
+            secondClick.removeEventListener('click', flipCard);
+        } else {
+            setTimeout(() => {
+                firstClick.classList.remove('flip');
+                secondClick.classList.remove('flip');
+            }, 1500);
             
-            
-            
-            console.log(firstClick);
-            console.log(secondClick);
         }
-)
+    }
+    cells.forEach(cell => cell.addEventListener('click', flipCard));
 
 
-// create game board
-function board() {
-    for(let i = 0; i < emojis.length; i++);
-    let card = document.createElement('text');
-    card.setAttribute('emojis');
-    card.setAttribute('data-id', i);
-    card.addEventListener('click', flipCard);
-    board.appendChild(card);
-}
-
-// reseting start timer back to 45 seconds after game ends
-resetButton.onclick = reloadPage;
-
-function reloadPage() {
-    window.location.reload();
-}
-
-// declaring matched cards
-let counterTwo = 1;
-let wrong = false;
-for (let i = 0; i < emojis.length; i++) {
-    // emojis[i].addEventListener('click');
-}
-function proceed() {
-    firstClick.classList.toggle('open');
-    secondClick.classList.toggle('open');
-    wrong = false;
-}
-
-}, {once: true})})
+    }
